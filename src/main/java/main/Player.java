cc4bd7527e379a5fa10d83e67d4620cee047ccaa
Zlodiak <evil.aaz@gmail.com>
2017-09-19 20:19:24 +0300

    логирование гребанного начала раскопок

--- src/main/java/main/Player.java
+++ src/main/java/main/Player.java
@@ -2795,7 +2795,7 @@ public class Player {
             ResultSet rs = query.executeQuery();
             if (!rs.isBeforeFirst()) {
                 jresult.put("Result","O2101");
-                Logwrite("startExtract","GUID="+GUID+", resLatSize="+Params.resLatSize+", TLAT / Params.resLatSize="+Integer.toString(TLAT / Params.resLatSize)+", Params.resLngSize="+Params.resLngSize+", TLNG / Params.resLngSize="+Integer.toString(TLAT / Params.resLngSize));
+                Logwrite("startExtract","GUID="+GUID+", TLAT="+TLAT+", TLNG="+TLNG+", Params.resLatSize="+Params.resLatSize+", TLAT / Params.resLatSize="+Integer.toString(TLAT / Params.resLatSize)+", Params.resLngSize="+Params.resLngSize+", TLNG / Params.resLngSize="+Integer.toString(TLAT / Params.resLngSize));
                 return jresult.toString();
             }
         }
